787ec03dbcafce0aee0d68279dfae8585ee2d30b
Greg P <gorgarp@gmail.com>
2023-01-22 14:14:13 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import openai_secret_manager
+import requests
+
+# Authenticate with the OpenAI API
+secrets = openai_secret_manager.get_secrets("openai")
+api_key = secrets["api_key"]
+headers = {"Authorization": f"Bearer {api_key}"}
+
+# Define the folder containing the files to be analyzed
+folder_path = "path/to/folder"
+
+# Iterate through the files in the folder
+for filename in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filename)
+    with open(file_path, "r") as file:
+        file_contents = file.read()
+
+        # Submit the file contents to the OpenAI API for code analysis
+        data = {"prompt": f"find code errors in this file {file_contents}"}
+        response = requests.post("https://api.openai.com/v1/engines/davinci/completions", json=data, headers=headers)
+        response_text = response.text
+
+        # Extract the code errors from the API response
+        errors = extract_errors_from_response(response_text)
+
+        # Add the errors to the report
+        for error in errors:
+            report.append(f"{filename}: {error}")
+
+# Save the report to a file
+with open("code_errors_report.txt", "w") as report_file:
+    for item in report:
+        report_file.write("%s\n" % item)
